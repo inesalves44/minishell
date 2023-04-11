6267ff511b5efff3903c28b86d046b9dbfffdb80
heitormp <heitormp@protonmail.com>
2023-04-11 07:51:45 +0100

    heitor:-11/04-fixed-export-errors

--- final product/built-in/built_export.c
+++ final product/built-in/built_export.c
@@ -6,24 +6,30 @@
 /*   By: hmaciel- <hmaciel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 21:47:57 by hmaciel-          #+#    #+#             */
-/*   Updated: 2023/04/10 22:06:43 by hmaciel-         ###   ########.fr       */
+/*   Updated: 2023/04/11 07:46:59 by hmaciel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static int	has_error(const char *arg)
+static int	has_error(char *key, char *value)
 {
 	int		i;
 
 	i = -1;
-	if (arg[0] == '$' || arg[0] == '=')
-	{
-		ft_putstr_fd("minishell: export: `", STDERR);
-		while (arg[++i])
-			write(STDERR, &arg[i], 1);
-		ft_putstr_fd("\': not a valid identifier\n", STDERR);
+	if (!key)
 		return (TRUE);
+	while (key[++i])
+	{
+		if (!ft_isalnum(key[i]))
+		{
+			ft_putstr_fd("minishell: export: `", STDERR);
+			ft_putstr_fd(key, STDERR);
+			ft_putstr_fd("=", STDERR);
+			ft_putstr_fd(value, STDERR);
+			ft_putstr_fd("\': not a valid identifier\n", STDERR);
+			return (TRUE);
+		}
 	}
 	return (FALSE);
 }
@@ -39,16 +45,14 @@ int	export(t_root *root)
 	cmd = 1;
 	while (root->tree->command[cmd])
 	{
-		if (has_error(root->tree->command[cmd]))
+		key = get_key_from_str(root->tree->command[cmd]);
+		value = get_value_from_str(root->tree->command[cmd]);
+		if (has_error(key, value))
 			ret = 1;
 		else if (ft_strchr(root->tree->command[cmd], '=') != NULL)
 		{
-			key = get_key_from_str(root->tree->command[cmd]);
-			value = get_value_from_str(root->tree->command[cmd]);
 			if (!change_value(root, key, value))
-			{
 				ft_lstadd_back_env(&root->env_lst, ft_lstnew_env(key, value));
-			}
 		}
 		cmd++;
 	}
